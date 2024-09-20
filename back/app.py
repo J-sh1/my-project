9561910d4ac8fd95a3d91a0dbb89e22a8119579a
J-sh1 <j2000star@kakao.com>
2024-09-20 20:02:45 +0900

    중간저장

--- back/app.py
+++ back/app.py
@@ -1,10 +1,23 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+# 다른 모듈에서 블루프린트 가져오기
+from routes.user import user_bp
+
+load_dotenv()
 
 app = Flask(__name__)
+# 환경변수에서 CORS 허용 출처 가져오기
+CORS_ORIGIN = os.getenv('CORS_ORIGIN', '*')
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    return jsonify({"message": "Hello from Flask!"})
+# CORS 설정 - 환경변수에서 가져온 출처만 허용
+CORS(app, resources={r'/*': {"origins": CORS_ORIGIN}})
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+# 블루프린트 등록
+app.register_blueprint(user_bp)
 
 if __name__ == '__main__':
     app.run(port = 5000, debug=True)
