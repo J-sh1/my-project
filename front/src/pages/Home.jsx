4c64db03999d9e7fa0a49e6ffadc98f61619d755
J-sh1 <j2000star@kakao.com>
2024-09-27 19:32:59 +0900

    .

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import '../css/home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className='main-container'>
+        <div className="main-container-box">1</div>
+        <div className="main-container-box">2</div>
+        <div className="main-container-box">3</div>
+        <div className="main-container-box">4</div>
+      </div>
+    </div>
   )
 }
 
