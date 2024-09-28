0f94767fca9d48ac6687b8dcdaf06f7e877a4d34
J-sh1 <j2000star@kakako.com>
2024-09-28 16:34:12 +0900

    메인페이지 영역분리

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import '../css/home.css'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
       <div className='main-container'>
-        <div className="main-container-box">1</div>
-        <div className="main-container-box">2</div>
-        <div className="main-container-box">3</div>
-        <div className="main-container-box">4</div>
+        <div className="main-container-box box1">
+          <div>
+            <span>분류</span>
+            <span>제목</span>
+            <Link to={'/login'}>더보기▶</Link>
+          </div>
+          <div></div> {/* 밑줄 */}
+          <div>1</div>
+        </div>
+        <div className="main-container-box box2">
+          <div>1</div>
+        </div>
+        <div className="main-container-box box3">
+          <div>1</div>
+        </div>
+        <div className="main-container-box box4">
+          <div>1</div>
+        </div>
       </div>
     </div>
   )
